0dee234c89332827fa95d72ee2fd1ea68fd01b7b
Khristinasheer <khristinasar@gmail.com>
2020-11-24 15:30:34 -0400

    Update copy

--- components/local-sellers.js
+++ components/local-sellers.js
@@ -4,31 +4,31 @@ const LocalSellers = () => {
   const data = {
     title: "Local Sellers",
     description:
-      "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?",
+      "No matter what skill or trade you have to offer, My Lokaal makes it simple and secure to sell online. You have full control of your products, prices and communication.",
     cards: [
       {
         title: "Food",
-        alt: "food",
+        alt: "Jam with strawberries.",
         src:
           "https://images.unsplash.com/photo-1500912239908-4ee48acb3a7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
         text:
-          "Lorem eference site about Lorem Ipsum, giving information on its origins, as well as a random",
+          "From baked goods, prepped home meals for pickup or private in-home cooks, MyLokaal gives you the tools to start earning money using your culinary skills.",
       },
       {
         title: "Services",
-        alt: "food",
+        alt: "Maintenance tools.",
         src:
           "https://images.unsplash.com/photo-1581783898377-1c85bf937427?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=958&q=80",
         text:
-          "Reference site about Lorem Ipsum, giving information on its origins, as well as a random",
+          "Whether you work remotely from home or provide an on-call serve, MyLokaal connects you with qualified customers with more repeat business.",
       },
       {
         title: "Products",
-        alt: "food",
+        alt: "Handmade organic soap.",
         src:
           "https://images.unsplash.com/photo-1605264964528-06403738d6dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
         text:
-          " Beference site about Lorem Ipsum, giving information on its origins, as well as a random",
+          "Sell original home-made products using MyLokaal to expand your reach locally, regionally and internationally!",
       },
     ],
   };
